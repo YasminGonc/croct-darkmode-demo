c01cd127d5fe1723df6da580a70a4bf2a83c84e1
Marcos Passos <marcos@croct.com>
2024-05-15 12:23:22 -0600

    Update examples

--- components/IdentityButton.tsx
+++ components/IdentityButton.tsx
@@ -5,8 +5,10 @@ import {identifyUser} from '@/app/actions';
 
 export const IdentityButton: FunctionComponent = () => (
     <button
+        type="button"
         onClick={
             async (): Promise<void> => {
+                // eslint-disable-next-line no-alert -- This is a demo
                 await identifyUser(prompt('Enter the user ID') ?? 'mp');
             }
         }
