4ea9574f21036f7d67a71279887929f12d548664
Marcos Passos <marcos@croct.com>
2024-05-16 09:19:57 -0600

    Revert config

--- app/page.tsx
+++ app/page.tsx
@@ -3,22 +3,6 @@ import {fetchContent} from '@croct/plug-next/server';
 import PricingSection from '@/components/PricingSection';
 import PrivacyNotice from '@/components/PrivacyNotice';
 
-/**
- * Agenda for today's demo
- *
- * Introduction
- * - What is this demo about?
- * - How is it built?
- *
- * Highlights
- *
- * 1. Localization: how to support multiple languages easily
- * 2. Dynamic content: how to generate content in real-time
- * 3. Auto-versioning: how to move fast and not break things
- * 4. Fallbacks: how to ensure your site will always work, even if Croct is down
- * 5. Context: how to share date with the personalization layer
- * 6. Evaluation: how to access the personalization layer from your code
- */
 export default async function PricingPage(): Promise<ReactElement> {
     const content = await fetchContent('pricing-page@2');
 
